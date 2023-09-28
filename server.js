398b370216054fb44a8bc14b9622b1097d2aee1b
Ole <ole@MBP-de-Ole.lan>
2023-09-28 23:16:32 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+/**
+ * Handle GET requests to the root URL
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ */
+app.get('/', (req, res) => {
+  res.send('Hello, World! I am the server!');
+});
+
+/**
+ * Start the server
+ */
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+//npm install -g nodemon
